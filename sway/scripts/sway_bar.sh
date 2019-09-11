eb344da4afd9275862be8efd63c669caed13dc45
Maxim Eryomenko <moeryomenko@gmail.com>
2019-09-11 08:51:03 +0300

    [sway/bar] remove redundant indicators
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

--- sway/scripts/sway_bar.sh
+++ sway/scripts/sway_bar.sh
@@ -25,13 +25,10 @@ audio_is_muted=$(amixer sget 'Master' | grep -e '[0-9][0-9]%' | head -1 | awk '{
 
 # Network
 network=$(ip route get 1.1.1.1 | grep -Po '(?<=dev\s)\w+' | cut -f1 -d ' ')
-# interface_easyname grabs the "old" interface name before systemd renamed it
 interface_easyname=$(dmesg | grep $network | grep renamed | awk 'NF>1{print $NF}')
-ping=$(ping -c 1 www.google.com | tail -1| awk '{print $4}' | cut -d '/' -f 2 | cut -d '.' -f 1)
 
 # Others
 language=$(swaymsg -r -t get_inputs | awk '/1:1:AT_Translated_Set_2_keyboard/;/xkb_active_layout_name/' | grep -A1 '\b1:1:AT_Translated_Set_2_keyboard\b' | grep "xkb_active_layout_name" | awk -F '"' '{print $4}')
-loadavg_5min=$(cat /proc/loadavg | awk -F ' ' '{print $2}')
 
 #weather=$(curl -Ss 'https://wttr.in/Krasnodar?0&T&Q&format=1')
 
@@ -56,4 +53,4 @@ else
     audio_active='🔊'
 fi
 
-echo "⌨ $language | $network_active $interface_easyname ($ping ms) | 🏋 $loadavg_5min | $audio_active $audio_volume | $battery_pluggedin $battery_charge% | $date_and_week $current_time"
+echo "⌨ $language | $network_active $interface_easyname | $audio_active $audio_volume | $battery_pluggedin $battery_charge% | $date_and_week $current_time"
