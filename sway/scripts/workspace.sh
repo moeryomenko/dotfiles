30c3cbf19f897b793a0402c9197c7861846fdae6
Maxim Eryomenko <moeryomenko@gmail.com>
2021-01-20 06:14:38 +0300

    [sway/scripts] add support for multi monitors
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

--- sway/scripts/workspace.sh
+++ sway/scripts/workspace.sh
@@ -1,6 +1,14 @@
-#!/usr/bin/bash  
+#!/usr/bin/env zsh
 
-current=$(swaymsg -t get_outputs | jq '.[] | .current_workspace' | sed 's/"//g')
+state=(`echo $(swaymsg -t get_outputs | jq -jr '.[] | .focused," ",.current_workspace," "' | sed 's/"//g'| sed 's/true/1/g' | sed 's/false/0/g') | sed 's/ /\n/g'`)
+
+current=`for i in $(seq 1 2 ${#state[@]}); do
+	if [[ $state[$i] == 1 ]]; then
+		echo $(($state[$i+1]))
+		break
+	fi
+done`
+
 case $1 in
 	"right")
 		next=$(($current+1))
@@ -9,4 +17,5 @@ case $1 in
 		next=$(($current-1))
 		if [[ $next == 0 ]]; then next=10; fi;;
 esac
+
 swaymsg workspace $next
