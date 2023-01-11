4e06d7df86fc2f7c852daa0fceae90237e274fa3
Maxim Eryomenko <moeryomenko@gmail.com>
2023-01-11 23:17:32 +0300

    feat(nvim): open dap ui on c++ debugging
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

--- nvim/.config/nvim/lua/debugging.lua
+++ nvim/.config/nvim/lua/debugging.lua
@@ -57,13 +57,13 @@ require("metals").setup_dap()
 
 local Path = require("plenary.path")
 require("tasks").setup({
-	default_params = { -- Default module parameters with which `neovim.json` will be created.
+	default_params = {
 		cmake = {
-			cmd = "cmake", -- CMake executable to use, can be changed using `:Task set_module_param cmake cmd`.
-			build_dir = tostring(Path:new("{cwd}", "build")), -- Build directory. The expressions `{cwd}`, `{os}` and `{build_type}` will be expanded with the corresponding text values. Could be a function that return the path to the build directory.
-			build_type = "Debug", -- Build type, can be changed using `:Task set_module_param cmake build_type`.
-			dap_name = "lldb", -- DAP configuration name from `require('dap').configurations`. If there is no such configuration, a new one with this name as `type` will be created.
-			args = { -- Task default arguments.
+			cmd = "cmake",
+			build_dir = tostring(Path:new("{cwd}", "build")),
+			build_type = "Debug",
+			dap_name = "lldb",
+			args = {
 				configure = {
 					"-D",
 					"CMAKE_EXPORT_COMPILE_COMMANDS=1",
@@ -85,13 +85,13 @@ require("tasks").setup({
 			},
 		},
 	},
-	save_before_run = true, -- If true, all files will be saved before executing a task.
-	params_file = "neovim.json", -- JSON file to store module and task parameters.
+	save_before_run = true,
+	params_file = "neovim.json",
 	quickfix = {
-		pos = "botright", -- Default quickfix position.
-		height = 12, -- Default height.
+		pos = "botright",
+		height = 12,
 	},
 	dap_open_command = function()
-		return dap.repl.open()
-	end, -- Command to run after starting DAP session. You can set it to `false` if you don't want to open anything or `require('dapui').open` if you are using https://github.com/rcarriga/nvim-dap-ui
+		return require("dapui").open()
+	end,
 })
