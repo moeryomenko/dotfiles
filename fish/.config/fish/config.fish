1d5d6b0f9f8145526ca2d40fe49bafec705df077
Maxim Eryomenko <moeryomenko@gmail.com>
2023-02-05 01:14:56 +0300

    chore(fish): update configuration
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,16 +5,22 @@ end
 
 set -U EDITOR nvim
 set -U NPM_CONFIG_PREFIX $HOME/.npm-global
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
 fish_add_path $NPM_CONFIG_PREFIX/bin
 fish_add_path $HOME/.config/git-commands
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.cargo/bin
-set -U MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
 set -U tide_git_icon 
 set -U tide_pwd_icon 
 set -U tide_pwd_icon_home 
+
+# wayland env vars.
+set -U GDK_BACKEND wayland
+set -U XDG_SESSION_TYPE wayland
+set -U XDG_CURRENT_DESKTOP sway
+set -U MOZ_ENABLE_WAYLAND 1
 
 alias ll='exa -l -h --git --classify --icons'
 alias la='ll -a'
@@ -24,10 +30,6 @@ if set -q KITTY_INSTALLATION_DIR
     set --global KITTY_SHELL_INTEGRATION enabled
     source "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish"
     set --prepend fish_complete_path "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_completions.d"
-end
-
-if status is-login
-    keychain --clear --quiet
 end
 
 if test -f ~/.keychain/(prompt_hostname)-gpg-fish
@@ -41,4 +43,11 @@ end
 if status is-interactive
 	keychain --eval --agents ssh -Q --quiet ~/.ssh/id_moeryomenko
 	keychain --eval --agents gpg --quiet --gpg2 BDEFC42C5E88B8C5
+end
+
+if status --is-login
+    keychain --clear --quiet
+	if test (tty) = /dev/tty1
+		exec sway
+	end
 end
