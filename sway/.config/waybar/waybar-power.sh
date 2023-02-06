331e9568f558783d6a34159beb5beae63819ae81
Maxim Eryomenko <moeryomenko@gmail.com>
2023-02-06 19:01:56 +0300

    feat(waybar): add hibernate button for poweroff notification
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

--- sway/.config/waybar/waybar-power.sh
+++ sway/.config/waybar/waybar-power.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-swaynag -t custom -m 'Are you sure you want to shutdown your device?' -b 'Poweroff' 'poweroff'
+swaynag -m 'Do you want shutdown or hibernate?' -b 'Poweroff' 'poweroff' -b 'Hibernate' 'systemctl hibernate'
