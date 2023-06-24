79e43b4610481b7b7c6e095e02e57ac3f27467db
Maxim Eryomenko <moeryomenko@gmail.com>
2023-06-24 09:09:49 +0300

    chore(fish): setup configuration for workstation
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,7 @@
-if status --is-interactive
-    set -lx SHELL fish
-    keychain --eval --agents ssh --quiet -Q ~/.ssh/id_ed25519 | source
-    keychain --eval --agents gpg --quiet --gpg2 -Q BDEFC42C5E88B8C5 | source
-end
-
-if not test -f ~/.config/fish/fish_plugins
-  curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-  fisher install IlanCosman/tide@v5
+if status is-interactive
+	set -lx SHELL fish
+	keychain --eval --agents ssh --quiet -Q ~/.ssh/id_ed25519 | source
+	keychain --eval --agents gpg --quiet --gpg2 -Q BDEFC42C5E88B8C5 | source
 end
 
 if not test -d $HOME/.asdf
@@ -15,23 +10,26 @@ end
 source $HOME/.asdf/asdf.fish
 source $HOME/.asdf/completions/asdf.fish
 
+if not test -d $HOME/.sdkman
+		curl -s "https://get.sdkman.io" | bash
+		fisher install reitzig/sdkman-for-fish@v1.4.0
+end
+
+if test -d $HOME/.sdkman
+        export SDKMAN_DIR="$HOME/.sdkman"
+        export JAVA_HOME=$(sdk home java current)
+end
+
 set -U EDITOR nvim
 set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
-fish_add_path $NPM_CONFIG_PREFIX/bin
 fish_add_path $HOME/.config/git-commands
 fish_add_path $HOME/.local/bin
-fish_add_path (rustup show home)/bin
+# fish_add_path (rustup show home)/bin
 
 set -U tide_git_icon 󰊢
 set -U tide_pwd_icon 󰉋
 set -U tide_pwd_icon_home 󱂵
-
-# wayland env vars.
-set -U GDK_BACKEND wayland
-set -U XDG_SESSION_TYPE wayland
-set -U XDG_CURRENT_DESKTOP sway
-set -U MOZ_ENABLE_WAYLAND 1
 
 alias ll='exa -l -h --git --classify --icons'
 alias la='ll -a'
@@ -46,16 +44,5 @@ end
 function fz
   sk --preview 'bat --color=always --style=numbers --line-range=:500 {}' --preview-window=right:70%
 end
-
-if test (tty) = /dev/tty1
-	export RADV_VIDEO_DECODE=1
-	export SDL_VIDEODRIVER=wayland
-	export GDK_BACKEND=wayland
-	export XDG_SESSION_TYPE=wayland
-	export XDG_CURRENT_DESKTOP=sway
-	export MOZ_ENABLE_WAYLAND=1
-	exec sway
-end
-
 
 alias vf='nvim (fz)'
