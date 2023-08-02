e873c69525a027bff363983498addc7dc26e6fa8
Maxim Eryomenko <moeryomenko@gmail.com>
2023-08-02 12:16:48 +0300

    feat(nvim): add database connection with ui
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

---
+++ nvim/.config/nvim/lua/db.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+function M.setup()
+	vim.g.db_ui_save_location = vim.fn.stdpath("config") .. require("plenary.path").path.sep .. "db_ui"
+
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = {
+			"sql",
+		},
+		command = [[setlocal omnifunc=vim_dadbod_completion#omni]],
+	})
+
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = {
+			"sql",
+			"mysql",
+			"plsql",
+		},
+		callback = function()
+			vim.schedule(db_completion)
+		end,
+	})
+end
+
+return M
