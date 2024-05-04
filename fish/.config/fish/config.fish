fae58f014ce051d3178415a47ca78c51b9a5a476
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2024-05-04 12:52:30 +0300

    feat(hyprland): use natural scroll for touchpad
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -44,10 +44,10 @@ alias hx='helix'
 
 function worktree
     git worktree list | awk '{ print $1}' | \
-    	sk --ansi --no-sort --reverse --tiebreak=index \
-    	--preview "git -C {} log --no-merges --date=relative -p" \
-    	--bind "alt-j:preview-down,alt-k:preview-up,ctrl-f:preview-page-down,crtl-b:preview-page-up,q:abort,ctrl-m:exec:echo {}" \
-    	--preview-window=right:70%
+		sk --ansi --no-sort --reverse --tiebreak=index \
+		--preview "git -C {} log --no-merges --date=relative -p" \
+		--bind "alt-j:preview-down,alt-k:preview-up,ctrl-f:preview-page-down,crtl-b:preview-page-up,q:abort,ctrl-m:exec:echo {}" \
+		--preview-window=right:70%
 end
 
 function cscope_gen
