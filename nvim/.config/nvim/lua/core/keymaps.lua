9bd69f79e57f569b61db93904b1474fdf235bad6
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2024-05-12 18:23:51 +0300

    refactor(nvim): add utils module for common functions
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -1,10 +1,4 @@
-local function map(mode, lhs, rhs, opts)
-	local options = { noremap = true, silent = true }
-	if opts then
-		options = vim.tbl_extend("force", options, opts)
-	end
-	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
+local map = require("core.utils").map
 
 -- close other buffers, except for the current.
 map("n", "<leader>co", ':%bdelete|edit #|normal `"<CR>')
