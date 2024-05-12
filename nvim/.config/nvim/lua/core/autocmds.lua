ee9c564d7627123eeb3df5bd1173c3eebe59e4d7
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2024-05-12 11:08:22 +0300

    feat(nvim): close some buffers by <q>
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

--- nvim/.config/nvim/lua/core/autocmds.lua
+++ nvim/.config/nvim/lua/core/autocmds.lua
@@ -97,3 +97,27 @@ vim.api.nvim_create_autocmd("Filetype", {
 		vim.opt.colorcolumn = "80"
 	end,
 })
+
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("close_with_q", { clear = true }),
+	pattern = {
+		"PlenaryTestPopup",
+		"help",
+		"lspinfo",
+		"man",
+		"notify",
+		"qf",
+		"spectre_panel",
+		"startuptime",
+		"tsplayground",
+		"neotest-output",
+		"checkhealth",
+		"neotest-summary",
+		"neotest-output-panel",
+	},
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+	end,
+})
