d83e5d957a2380c38fdab29949c91d6b58dfc310
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2024-05-13 09:16:49 +0300

    feat(nvim): add c++ configuration
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

---
+++ nvim/.config/nvim/lua/lang/cpp.lua
@@ -0,0 +1,90 @@
+return {
+	{
+		"stevearc/conform.nvim",
+		dependencies = {
+			{
+				"williamboman/mason.nvim",
+				opts = function(_, opts)
+					opts.ensure_installed = opts.ensure_installed or {}
+					vim.list_extend(opts.ensure_installed, { "clang-format" })
+				end,
+			},
+		},
+		ft = "cpp",
+		opts = {
+			formatters_by_ft = {
+				cpp = { "clang-format" },
+			},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{
+				"williamboman/mason-lspconfig.nvim",
+				dependencies = {
+					{
+						"williamboman/mason.nvim",
+					},
+					{
+						"artemave/workspace-diagnostics.nvim",
+						enabled = true,
+					},
+				},
+				opts = function(_, opts)
+					opts.ensure_installed = opts.ensure_installed or {}
+					vim.list_extend(opts.ensure_installed, { "clangd" })
+				end,
+			},
+		},
+		ft = "cpp",
+	},
+	{
+		"https://git.sr.ht/~p00f/clangd_extensions.nvim",
+		ft = "cpp",
+		opts = {
+			autoSetHints = true,
+			inlay_hints = {
+				only_current_line = true,
+				only_current_line_autocmd = "CursorHold",
+				show_parameter_hints = true,
+				parameter_hints_prefix = "<- ",
+				other_hints_prefix = "=> ",
+				max_len_align = false,
+				max_len_align_padding = 1,
+				right_align = false,
+				right_align_padding = 7,
+				highlight = "Comment",
+				priority = 100,
+			},
+			ast = {
+				role_icons = {
+					type = "",
+					declaration = "",
+					expression = "",
+					specifier = "",
+					statement = "",
+					["template argument"] = "",
+				},
+				kind_icons = {
+					Compound = "",
+					Recovery = "",
+					TranslationUnit = "",
+					PackExpansion = "",
+					TemplateTypeParm = "",
+					TemplateTemplateParm = "",
+					TemplateParamObject = "",
+				},
+				highlights = {
+					detail = "Comment",
+				},
+				memory_usage = {
+					border = "none",
+				},
+				symbol_info = {
+					border = "none",
+				},
+			},
+		},
+	},
+}
