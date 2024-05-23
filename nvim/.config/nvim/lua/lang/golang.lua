246dd2aafa11c3b34128476d6cdbe877a5abb3eb
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2024-05-23 14:05:13 +0300

    chore(nvim): remove neotest plugin
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

--- nvim/.config/nvim/lua/lang/golang.lua
+++ nvim/.config/nvim/lua/lang/golang.lua
@@ -76,40 +76,6 @@ return {
 		ft = { "go", "gomod", "gowork", "gotmpl" },
 	},
 	{
-		"nvim-neotest/neotest",
-		ft = { "go" },
-		dependencies = {
-			"fredrikaverpil/neotest-golang",
-		},
-		opts = function(_, opts)
-			opts.adapters = opts.adapters or {}
-			opts.adapters["neotest-golang"] = {
-				go_test_args = {
-					"-v",
-					"-race",
-					"-count=1",
-					"-timeout=60s",
-					"-coverprofile=" .. vim.fn.getcwd() .. "/coverage.out",
-				},
-				dap_go_enabled = true,
-			}
-		end,
-	},
-	{
-		"andythigpen/nvim-coverage",
-		ft = { "go" },
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {
-			-- https://github.com/andythigpen/nvim-coverage/blob/main/doc/nvim-coverage.txt
-			auto_reload = true,
-			lang = {
-				go = {
-					coverage_file = vim.fn.getcwd() .. "/coverage.out",
-				},
-			},
-		},
-	},
-	{
 		"mfussenegger/nvim-dap",
 		ft = { "go" },
 		dependencies = {
