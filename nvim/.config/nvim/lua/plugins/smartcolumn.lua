4079a8047a168872b5c006cc38c0003eb8ae6c89
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2024-05-26 18:30:53 +0300

    feat(nvim): add smartcolumn
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

---
+++ nvim/.config/nvim/lua/plugins/smartcolumn.lua
@@ -0,0 +1,17 @@
+return {
+	"m4xshen/smartcolumn.nvim",
+	opts = {
+		disabled_filetypes = {
+			"netrw",
+			"NvimTree",
+			"Lazy",
+			"mason",
+			"help",
+			"text",
+			"markdown",
+			"tex",
+			"html",
+		},
+		scope = "window",
+	},
+}
