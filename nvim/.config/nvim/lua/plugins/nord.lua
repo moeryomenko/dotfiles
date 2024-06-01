aa2aad8274099cd4e503bb70ef0b368fc645e80d
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2024-06-01 10:49:06 +0300

    chore(nvim): use native for nvim nord colorscheme
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

--- nvim/.config/nvim/lua/plugins/nord.lua
+++ nvim/.config/nvim/lua/plugins/nord.lua
@@ -1,10 +1,9 @@
 return {
-	"nordtheme/vim",
+	"gbprod/nord.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		-- Optionally configure and load the colorscheme
-		-- directly inside the plugin declaration.
+		require("nord").setup({})
 		vim.cmd.colorscheme("nord")
 	end,
 }
