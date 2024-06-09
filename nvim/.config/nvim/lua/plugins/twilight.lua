359abe3dd840ac86598f4f28a24a6c7efde20562
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2024-06-09 14:06:28 +0300

    feat(nvim): add zen-mode
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

---
+++ nvim/.config/nvim/lua/plugins/twilight.lua
@@ -0,0 +1,14 @@
+return {
+	"folke/twilight.nvim",
+	{
+		"folke/zen-mode.nvim",
+		cmd = "ZenMode",
+		opts = {
+			plugins = {
+				gitsigns = true,
+				tmux = true,
+			},
+		},
+		keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
+	},
+}
