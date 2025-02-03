df4f1dbfd5911f7b6e393237519bb3714eef839d
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2025-02-03 16:17:43 +0300

    feat(swaybg): change the wallpaper more often
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

--- sway/.config/sway/bin/wallpaper.sh
+++ sway/.config/sway/bin/wallpaper.sh
@@ -5,7 +5,7 @@ while true; do
 	swaybg -i `find $HOME/pictures/wallpapers/ -type f | shuf -n1` -m fill &
 	sleep 1
 	kill $PID
-	sleep 300
+	sleep 30
 done
 
 # mpvpaper -o "--volume=30" -v HDMI-A-1 ~/videos/dark_outpost_2.mkv &
