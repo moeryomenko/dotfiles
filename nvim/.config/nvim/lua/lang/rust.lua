2fb7840308a783e36707b9d4d54032d57dd37c98
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2025-02-24 10:45:00 +0300

    chore(nvim): remove unused plugin
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

--- nvim/.config/nvim/lua/lang/rust.lua
+++ nvim/.config/nvim/lua/lang/rust.lua
@@ -16,50 +16,6 @@ return {
 			table.insert(opts.sources, { name = "crates" })
 		end,
 	},
-	{
-		"mrcjkb/rustaceanvim",
-		version = "^4", -- Recommended
-		ft = { "rust" },
-		opts = {
-			server = {
-				on_attach = function(_, bufnr)
-					vim.keymap.set("n", "<leader>ca", function()
-						vim.cmd.RustLsp("codeAction")
-					end, { desc = "Code Action", buffer = bufnr })
-					vim.keymap.set("n", "<leader>dr", function()
-						vim.cmd.RustLsp("debuggables")
-					end, { desc = "Rust Debuggables", buffer = bufnr })
-				end,
-				default_settings = {
-					-- rust-analyzer language server configuration
-					["rust-analyzer"] = {
-						cargo = {
-							allFeatures = true,
-							loadOutDirsFromCheck = true,
-							runBuildScripts = true,
-						},
-						-- Add clippy lints for Rust.
-						checkOnSave = {
-							allFeatures = true,
-							command = "clippy",
-							extraArgs = { "--no-deps" },
-						},
-						procMacro = {
-							enable = true,
-							ignored = {
-								["async-trait"] = { "async_trait" },
-								["napi-derive"] = { "napi" },
-								["async-recursion"] = { "async_recursion" },
-							},
-						},
-					},
-				},
-			},
-		},
-		config = function(_, opts)
-			vim.g.rustaceanvim = vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, opts or {})
-		end,
-	},
 	-- Ensure Rust debugger is installed
 	{
 		"williamboman/mason.nvim",
