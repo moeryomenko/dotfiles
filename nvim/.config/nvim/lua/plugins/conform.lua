5d40feb4d50c6ed6e2f5782f4839803e5e178610
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2025-03-11 19:11:27 +0300

    feat(nvim): adopt conform configure from lang
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -5,22 +5,34 @@ return {
 	init = function()
 		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 	end,
-	config = function()
-		require("conform").setup({
+	config = function(_, opts)
+		local conf = {
 			formatters_by_ft = {
-				go = { "goimports", "gofumpt" },
 				lua = { "stylua" },
 				proto = { "buf" },
 				python = { "black" },
 				cmake = { "gersemi" },
 				yaml = { "yamlfmt" },
 			},
+			formatters = {},
 			format_on_save = {
 				timeout_ms = 500,
 				lsp_fallback = true,
 			},
 			log_level = vim.log.levels.INFO,
 			notify_on_error = true,
-		})
+		}
+
+		if opts.formatters_by_ft ~= nil then
+			local merged = require("core.utils").deep_tbl_extend(conf.formatters_by_ft, opts.formatters_by_ft)
+			conf.formatters_by_ft = merged
+		end
+
+		if opts.formatters ~= nil then
+			local merged = require("core.utils").deep_tbl_extend(conf.formatters, opts.formatters)
+			conf.formatters = merged
+		end
+
+		require("conform").setup(conf)
 	end,
 }
