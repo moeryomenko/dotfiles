0fcfed140370cce8a5bbe02ecfd75ab1e7c7f25c
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2025-03-11 09:25:41 +0300

    feat(nvim): setup neotest plugin for configuration by lang
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

--- nvim/.config/nvim/lua/plugins/neotest.lua
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -1,36 +1,131 @@
 return {
 	"nvim-neotest/neotest",
+	event = "VeryLazy",
 	dependencies = {
 		"nvim-neotest/nvim-nio",
-		"nvim-neotest/neotest-go",
 		"nvim-lua/plenary.nvim",
 		"antoinemadec/FixCursorHold.nvim",
 		"nvim-treesitter/nvim-treesitter",
+
+		"nvim-neotest/neotest-plenary",
+		"nvim-neotest/neotest-vim-test",
 	},
-	keys = {
-		{ "<leader>nt", ":lua require('neotest').run.run()<CR>" },
-		{ "<leader>nto", ":lua require('neotest').output.open({ enter = true })<CR>" },
-	},
-	config = function()
-		-- get neotest namespace (api call creates or returns namespace)
-		local neotest_ns = vim.api.nvim_create_namespace("neotest")
-		vim.diagnostic.config({
-			virtual_text = {
-				format = function(diagnostic)
-					local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
-					return message
-				end,
-			},
-		}, neotest_ns)
-		require("neotest").setup({
-			adapters = {
-				require("neotest-go")({
-					experimental = {
-						test_table = true,
-					},
-					args = { "-count=1", "-timeout=60s" },
-				}),
-			},
-		})
+	config = function(_, opts)
+		if opts.adapters then
+			local adapters = {}
+			for name, config in pairs(opts.adapters or {}) do
+				if type(name) == "number" then
+					if type(config) == "string" then
+						config = require(config)
+					end
+					adapters[#adapters + 1] = config
+				elseif config ~= false then
+					local adapter = require(name)
+					if type(config) == "table" and not vim.tbl_isempty(config) then
+						local meta = getmetatable(adapter)
+						if adapter.setup then
+							adapter.setup(config)
+						elseif adapter.adapter then
+							adapter.adapter(config)
+							adapter = adapter.adapter
+						elseif meta and meta.__call then
+							adapter(config)
+						else
+							error("Adapter " .. name .. " does not support setup")
+						end
+					end
+					adapters[#adapters + 1] = adapter
+				end
+			end
+			opts.adapters = adapters
+		end
+
+		require("neotest").setup(opts)
 	end,
+	keys = {
+		{
+			"<leader>ta",
+			function()
+				require("neotest").run.attach()
+			end,
+			desc = "[t]est [a]ttach",
+		},
+		{
+			"<leader>tf",
+			function()
+				require("neotest").run.run(vim.fn.expand("%"))
+			end,
+			desc = "[t]est run [f]ile",
+		},
+		{
+			"<leader>tA",
+			function()
+				require("neotest").run.run(vim.uv.cwd())
+			end,
+			desc = "[t]est [A]ll files",
+		},
+		{
+			"<leader>tS",
+			function()
+				require("neotest").run.run({ suite = true })
+			end,
+			desc = "[t]est [S]uite",
+		},
+		{
+			"<leader>tn",
+			function()
+				require("neotest").run.run()
+			end,
+			desc = "[t]est [n]earest",
+		},
+		{
+			"<leader>tl",
+			function()
+				require("neotest").run.run_last()
+			end,
+			desc = "[t]est [l]ast",
+		},
+		{
+			"<leader>ts",
+			function()
+				require("neotest").summary.toggle()
+			end,
+			desc = "[t]est [s]ummary",
+		},
+		{
+			"<leader>to",
+			function()
+				require("neotest").output.open({ enter = true, auto_close = true })
+			end,
+			desc = "[t]est [o]utput",
+		},
+		{
+			"<leader>tO",
+			function()
+				require("neotest").output_panel.toggle()
+			end,
+			desc = "[t]est [O]utput panel",
+		},
+		{
+			"<leader>tt",
+			function()
+				require("neotest").run.stop()
+			end,
+			desc = "[t]est [t]erminate",
+		},
+		{
+			"<leader>td",
+			function()
+				require("neotest").run.run({ suite = false, strategy = "dap" })
+			end,
+			desc = "Debug nearest test",
+		},
+		{
+			"<leader>tD",
+			function()
+				require("neotest").run.run({ vim.fn.expand("%"), strategy = "dap" })
+			end,
+			desc = "Debug current file",
+		},
+	},
 }
