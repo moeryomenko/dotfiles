5372356229386aeabdf93e994662d1fcbd4e6071
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2025-06-12 01:22:36 +0300

    chore(config): set tokyo_night_enhanced theme and colors
    - Updated Alacritty to import `tokyo_night_enhanced` theme
    - Customized Neovim's Tokyo Night colors to match theme
    - Adjusted tmux color configurations for consistent theming
    
    This change ensures a cohesive visual experience across terminal
    applications by aligning all configurations with the Tokyo Night Enhanced
    theme.
    
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

--- nvim/.config/nvim/lua/plugins/tokyonight.lua
+++ nvim/.config/nvim/lua/plugins/tokyonight.lua
@@ -3,6 +3,46 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
+		local styles = require("tokyonight.colors").styles
+
+		styles.custom = {
+			bg = "#08080b", -- Updated from #24283b (primary.background)
+			bg_dark = "#1f2335",
+			bg_dark1 = "#1b1e2d",
+			bg_highlight = "#292e42",
+			blue = "#7aa2f7", -- Matches normal.blue/bright.blue
+			blue0 = "#3d59a1",
+			blue1 = "#2ac3de",
+			blue2 = "#0db9d7",
+			blue5 = "#89ddff",
+			blue6 = "#b4f9f8",
+			blue7 = "#394b70",
+			comment = "#565f89",
+			cyan = "#7dcfff", -- Matches normal.cyan/bright.cyan
+			dark3 = "#545c7e",
+			dark5 = "#737aa2",
+			fg = "#787c99", -- Updated from #c0caf5 (primary.foreground)
+			fg_dark = "#a9b1d6",
+			fg_gutter = "#3b4261",
+			green = "#41a6b5", -- Updated from #9ece6a (normal.green/bright.green)
+			green1 = "#73daca",
+			green2 = "#41a6b5", -- Already matches normal.green
+			magenta = "#bb9af7", -- Matches normal.magenta/bright.magenta
+			magenta2 = "#ff007c",
+			orange = "#ff9e64",
+			purple = "#9d7cd8",
+			red = "#f7768e", -- Matches normal.red/bright.red
+			red1 = "#db4b4b",
+			teal = "#1abc9c",
+			terminal_black = "#363b54", -- Updated from #414868 (normal.black/bright.black)
+			yellow = "#e0af68", -- Matches normal.yellow/bright.yellow
+			git = {
+				add = "#449dab",
+				change = "#6183bb",
+				delete = "#914c54",
+			},
+		}
+
 		require("tokyonight").setup({
 			on_highlights = function(hl, c)
 				local prompt = "#2d3149"
@@ -34,10 +74,8 @@ return {
 					fg = c.bg_dark,
 				}
 			end,
-			on_colors = function(colors)
-				colors.fg = "#7dcfff"
-			end,
 		})
-		vim.cmd([[colorscheme tokyonight-storm]])
+
+		require("tokyonight").load({ style = "custom" })
 	end,
 }
