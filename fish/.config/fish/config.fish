e1803c9226280352da574edff2a3288789bae1ea
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2025-09-24 19:59:03 +0300

    feat(fish): reorganize abbreviations
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -78,27 +78,37 @@ fx --comp fish | source
 set -l xdg_data_home $XDG_DATA_HOME ~/.local/share
 set -gx --path XDG_DATA_DIRS $xdg_data_home[1]/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
 
-abbr --add ll         "eza -l -h --git --classify --icons"
-abbr --add la         "eza -l -h --git --classify --icons -a"
-abbr --add tree       "eza -l -h --git --classify --icons --long --tree"
-abbr --add g          "git"
-abbr --add k          "kubectl"
-abbr --add lg         "lazygit"
-abbr --add glog       "git dlog"
-abbr --add ur         "ls | xargs -P10 -I{} git -C {} pull"
-abbr --add nv         "nvim"
-abbr --add fz         "sk --preview 'bat --color=always --style=numbers --line-range=:500 {}' --preview-window=right:70%"
-abbr --add cdfz       "z (sk --preview 'bat --color=always --style=numbers --line-range=:500 {}' --preview-window=right:70%)"
-abbr --add check_ping "ping -c 1 -W 3 google.com"
-abbr --add vf         "vim (sk --preview 'bat --color=always --style=numbers --line-range=:500 {}' --preview-window=right:70%)"
-abbr --add nf         "nvim (sk --preview 'bat --color=always --style=numbers --line-range=:500 {}' --preview-window=right:70%)"
-abbr --add pkgclean   "sudo pacman -Rncs (pacman -Qdtq)"
-abbr --add pkgcache   "sudo pacman -Scc"
-abbr --add sw         "cd (worktree)"
-abbr --add jqcs       "jq 'map_keys(from_camel|to_snake)'"
+# directory navigation related abbreviations
+abbr --add ll   "eza -l -h --git --classify --icons"
+abbr --add la   "eza -l -h --git --classify --icons -a"
+abbr --add tree "eza -l -h --git --classify --icons --long --tree"
+abbr --add fz   "sk --preview 'bat --color=always --style=numbers --line-range=:500 {}' --preview-window=right:70%"
+abbr --add cdfz "z (sk --preview 'bat --color=always --style=numbers --line-range=:500 {}' --preview-window=right:70%)"
 
-abbr --add fmtgou     "git status --short | grep '[A|M]' | grep -E -o '[^ ]*\$' | grep '\.go\$' | xargs -I{} goimports -local (go list -m -f {{.Path}}) -w {}"
-abbr --add gotest     "gotestsum --format-hide-empty-pkg -f dots-v2 -- -p=1 -count=1 -timeout=1200s -coverprofile coverage.out "
+# git related abbreviations
+abbr --add g    "git"
+abbr --add lg   "lazygit"
+abbr --add glog "git dlog"
+abbr --add ur   "ls | xargs -P10 -I{} git -C {} pull"
+abbr --add sw   "cd (worktree)"
+
+# editing related abbreviations
+abbr --add nv "nvim"
+abbr --add vf "vim (sk --preview 'bat --color=always --style=numbers --line-range=:500 {}' --preview-window=right:70%)"
+abbr --add nf "nvim (sk --preview 'bat --color=always --style=numbers --line-range=:500 {}' --preview-window=right:70%)"
+
+# kubectl related abbreviations
+abbr --add k   "kubectl"
+abbr --add ksc "kubectl config use-context (kubectl config get-contexts -o name | sk --reverse --bind \"j:down,k:up,q:abort\")"
+abbr --add kdn "kubectl config set-context --current --namespace=(kubectl get namespaces -o name | cut -d/ -f2 | sk --reverse --bind \"j:down,k:up,q:abort\")"
+
+# golang related abbreviations
+abbr --add fmtgou "git status --short | grep '[A|M]' | grep -E -o '[^ ]*\$' | grep '\.go\$' | xargs -I{} goimports -local (go list -m -f {{.Path}}) -w {}"
+abbr --add gotest "gotestsum --format-hide-empty-pkg -f dots-v2 -- -p=1 -count=1 -timeout=1200s -coverprofile coverage.out "
+
+# other stuff
+abbr --add check_ping "ping -c 1 -W 3 google.com"
+abbr --add jqcs       "jq 'map_keys(from_camel|to_snake)'"
 
 alias hx='helix'
 
@@ -111,7 +121,7 @@ function worktree
 end
 
 function cscope_gen
-	find . -regex '.*\.\(c\|h\|cc\|hh\|cpp\|hpp\|cxx\|hxx\|hlsl\|glsl\|comp\|vert\|frag\)' > cscope.files
+	find -E . -regex '.*.(c|h|cc|hh|cpp|hpp|cxx|hxx|hlsl|glsl|comp|vert|frag)' > cscope.files
 	cscope -b -q -k
 end
 
