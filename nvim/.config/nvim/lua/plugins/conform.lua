827c94b99b6602ade21c4a27cc254a41bdffa3e7
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2025-09-25 21:36:31 +0300

    feat(nvim): return conform for formatting
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,48 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	init = function()
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
+	config = function(_, opts)
+		local conf = {
+			formatters_by_ft = {
+				go = { "gofumpt", "goimports", "gci", "golines" },
+			},
+			formatters = {
+				gofumpt = {
+					prepend_args = { "-extra" },
+				},
+				gci = {
+					args = {
+						"write",
+						"--skip-generated",
+						"-s",
+						"standard",
+						"-s",
+						"default",
+						"--skip-vendor",
+						"$FILENAME",
+					},
+				},
+				goimports = {
+					args = { "-srcdir", "$FILENAME" },
+				},
+				golines = {
+					-- golines will use goimports as base formatter by default which is slow.
+					-- see https://github.com/segmentio/golines/issues/33
+					prepend_args = { "--base-formatter=gofumpt", "--ignore-generated", "--tab-len=1", "--max-len=120" },
+				},
+			},
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_fallback = true,
+			},
+			log_level = vim.log.levels.INFO,
+			notify_on_error = true,
+		}
+
+		require("conform").setup(conf)
+	end,
+}
