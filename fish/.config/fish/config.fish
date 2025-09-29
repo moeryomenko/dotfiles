44f75b5cd5a9e1e1b36e7e0a7ec84b1d0e07d745
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2025-09-29 10:25:44 +0300

    feat(fish): add command for select task from background
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -131,6 +131,26 @@ function replace_all
 	rg -l $argv[1] . | xargs sed -i "s/$argv[1]/$argv[2]/g"
 end
 
+function job-select
+    set -l job_list (jobs)
+
+    if test -z "$job_list"
+        echo "No background jobs"
+        return
+    end
+
+    set -l selected (printf '%s\n' $job_list | sk --reverse --header="Select job to bring to foreground")
+
+    if test -n "$selected"
+        set -l group_id (echo $selected | awk '{print $2}')
+        if test -n "$group_id"
+            fg $group_id
+        end
+    end
+end
+
+alias jf='job-select'
+
 if test (tty) = /dev/tty1
 	export RADV_VIDEO_DECODE=1
 	export SDL_VIDEODRIVER=wayland
