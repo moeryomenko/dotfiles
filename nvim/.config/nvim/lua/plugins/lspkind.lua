83330bbb8267fd2c82d5d649554609fb169bcab1
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2025-10-06 16:32:15 +0300

    style(nvim): customize lsp completion popup menu
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

---
+++ nvim/.config/nvim/lua/plugins/lspkind.lua
@@ -0,0 +1,35 @@
+return {
+	"onsails/lspkind.nvim",
+	config = function()
+		require('lspkind').init({
+			mode = 'symbol_text',
+			symbol_map = {
+				Text = "󰉿",
+				Method = "󰆧",
+				Function = "󰊕",
+				Constructor = "",
+				Field = "󰜢",
+				Variable = "󰀫",
+				Class = "󰠱",
+				Interface = "",
+				Module = "",
+				Property = "󰜢",
+				Unit = "󰑭",
+				Value = "󰎠",
+				Enum = "",
+				Keyword = "󰌋",
+				Snippet = "",
+				Color = "󰏘",
+				File = "󰈙",
+				Reference = "󰈇",
+				Folder = "󰉋",
+				EnumMember = "",
+				Constant = "󰏿",
+				Struct = "󰙅",
+				Event = "",
+				Operator = "󰆕",
+				TypeParameter = "",
+			},
+		})
+	end,
+}
