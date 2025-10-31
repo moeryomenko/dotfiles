d1e107febd88a650b3b460892667b84d96c4001d
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2025-10-31 22:26:59 +0300

    feat(fish): allow hide some local configuration from repo
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,14 +1,9 @@
 set -U fish_greeting ""
 
-if status is-interactive
-    set -lx SHELL fish
-    keychain --eval --ssh-allow-forwarded --quiet /home/eryoma/.ssh/id_ed25519 | source
-    keychain --eval --ssh-allow-forwarded --quiet /home/eryoma/.ssh/2gis | source
-    keychain --eval --quiet --gpg2 5318919FE71A1E81 | source
-	keychain --eval --quiet --gpg2 73EFE8958507112E | source
+# Source local overrides if they exist (not tracked in git)
+if test -f ~/.config/fish/config.local.fish
+    source ~/.config/fish/config.local.fish
 end
-
-export XDG_CONFIG_HOME=$HOME/.config
 
 fzf_configure_bindings --directory=\cf --git_log=\ct --git_status=\cs --history=\cy --processes=\cp
 
