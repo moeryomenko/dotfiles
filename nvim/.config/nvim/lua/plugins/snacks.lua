02b5e4d2fe9632ef47291a96ae7fc17e0a0dc618
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2025-11-15 22:52:20 +0300

    feat(nvim): add QoL plugin snacks
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,19 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		bigfile = { enabled = true },
+		dashboard = { enabled = true },
+		explorer = { enabled = false },
+		indent = { enabled = true },
+		input = { enabled = true },
+		picker = { enabled = true },
+		notifier = { enabled = true },
+		quickfile = { enabled = true },
+		scope = { enabled = true },
+		scroll = { enabled = false },
+		statuscolumn = { enabled = true },
+		words = { enabled = true },
+	},
+}
