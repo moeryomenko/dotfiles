f4fdea9f883d343cac6ef1bda87ea794685cffd7
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2025-12-12 15:08:27 +0300

    feat(hyprland): replace dynamic wallpaper slideshow with static video wallpaper
    The video wallpaper is set to loop indefinitely at 40% volume, creating a
    peaceful background atmosphere without requiring frequent process restarts.
    
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

--- sway/.config/sway/bin/wallpaper.sh
+++ sway/.config/sway/bin/wallpaper.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-while true; do
-	PID=`pidof swaybg`
-	swaybg -i `find $HOME/pictures/wallpapers/ -type f | shuf -n1` -m fill &
-	sleep 1
-	kill $PID
-	sleep 30
-done
+# while true; do
+# 	PID=`pidof swaybg`
+# 	swaybg -i `find $HOME/pictures/wallpapers/ -type f | shuf -n1` -m fill &
+# 	sleep 1
+# 	kill $PID
+# 	sleep 30
+# done
 
+mpvpaper -vs -o "--loop-file=inf --volume=40" HDMI-A-1 ~/videos/dark_forest_rainy_sci-fi_ambient.mp4
 # mpvpaper -o "--volume=30" -v HDMI-A-1 ~/videos/dark_outpost_2.mkv &
 # mpvpaper -o "--volume=0" -v DP-1 ~/videos/dark_outpost_2.mkv &
