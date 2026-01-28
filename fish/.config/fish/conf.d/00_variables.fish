e9fb73ca9453421e72f25f525a37765dc3f2e680
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2026-01-28 22:57:58 +0300

    feat(fish): add opencode alies
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

--- fish/.config/fish/conf.d/00_variables.fish
+++ fish/.config/fish/conf.d/00_variables.fish
@@ -17,6 +17,7 @@ fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.cargo/bin
 fish_add_path $GOPATH/bin
 fish_add_path $HOME/projects/flutter/bin
+fish_add_path $HOME/.opencode/bin
 
 set -q KREW_ROOT; and set -gx PATH $PATH $KREW_ROOT/.krew/bin; or set -gx PATH $PATH $HOME/.krew/bin
 
