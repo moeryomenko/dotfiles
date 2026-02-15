ac02d89dd2b1852a4686bbb1e209323481b7b2b6
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2026-02-15 22:31:09 +0300

    feat(fish): add huggingface token to env
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

--- fish/.config/fish/conf.d/00_variables.fish
+++ fish/.config/fish/conf.d/00_variables.fish
@@ -11,6 +11,7 @@ set -Ux GOPATH (go env GOPATH)
 set NPM_PACKAGES "$HOME/.npm-packages"
 set PATH $PATH $NPM_PACKAGES/bin
 set MANPATH $NPM_PACKAGES/share/man $MANPATH
+set -Ux HF_TOKEN (pass show hf/access_token)
 
 fish_add_path $XDG_CONFIG_HOME/git-commands
 fish_add_path $HOME/.local/bin
