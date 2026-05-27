0e1ceddcd2006f630681609e8600a96c390cca34
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2026-05-27 22:35:11 +0300

    feat(hypr): open mpv in concrete workspace
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

--- hyprland/.config/hypr/lua/windowrules.lua
+++ hyprland/.config/hypr/lua/windowrules.lua
@@ -5,4 +5,11 @@ hl.window_rule({
 	fullscreen = true,
 })
 
+hl.window_rule({
+	name = "mpv-rules",
+	match = { class = "^(mpv)$" },
+	workspace = 9,
+	fullscreen = true,
+})
+
 hl.workspace_rule({ workspace = "w[1-10]", animation = "slidefadevert" })
