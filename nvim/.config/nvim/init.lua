8d6ad8d365b47bd9cbf11a008e94f4163ae45fcd
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2026-06-09 21:56:53 +0300

    style: new colors theme
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
 require("colors.nord").setup()
+||||||| parent of 1334be3 (style: new colors theme)
+require("colors.neon_dreams").setup()
+=======
+require("colors.noir").setup()
+>>>>>>> 1334be3 (style: new colors theme)
 require("core.options")
 require("core.functions")
 require("core.keymaps")
