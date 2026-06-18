ae2c5146b74a915bc70b1b80efc644339c33c7e0
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2026-06-18 07:17:20 +0300

    fish: add abbreviations for open selected file in helix
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

--- fish/.config/fish/conf.d/02_abbreviations.fish
+++ fish/.config/fish/conf.d/02_abbreviations.fish
@@ -1,22 +1,23 @@
 # directory navigation related abbreviations
-abbr --add ll   "eza -l -h --git --classify --icons"
-abbr --add la   "eza -l -h --git --classify --icons -a"
+abbr --add ll "eza -l -h --git --classify --icons"
+abbr --add la "eza -l -h --git --classify --icons -a"
 abbr --add tree "eza -l -h --git --classify --icons --long --tree"
-abbr --add fz   "sk --reverse --preview 'bat --color=always --style=numbers --line-range=:500 {}' --preview-window=right:70%"
+abbr --add fz "sk --reverse --preview 'bat --color=always --style=numbers --line-range=:500 {}' --preview-window=right:70%"
 abbr --add cdfz "z (sk --reverse --preview 'bat --color=always --style=numbers --line-range=:500 {}' --preview-window=right:70%)"
 
 # git related abbreviations
-abbr --add g    "git"
-abbr --add lg   "lazygit"
+abbr --add g git
+abbr --add lg lazygit
 abbr --add glog "git dlog"
-abbr --add ur   "ls | xargs -P10 -I{} git -C {} pull"
-abbr --add sw   "cd (worktree)"
+abbr --add ur "ls | xargs -P10 -I{} git -C {} pull"
+abbr --add sw "cd (worktree)"
 
 # editing related abbreviations
 abbr --add vf "v (sk --preview 'bat --color=always --style=numbers --line-range=:500 {}' --preview-window=right:70%)"
+abbr --add hxf "hx (sk --preview 'bat --color=always --style=numbers --line-range=:500 {}' --preview-window=right:70%)"
 
 # kubectl related abbreviations
-abbr --add k   "kubectl"
+abbr --add k kubectl
 abbr --add kdp "kubectl get po -o name | cut -d/ -f2 | sk --reverse --preview-window=right:75% --preview 'kubectl describe po {} | bat'"
 
 # golang related abbreviations
@@ -26,5 +27,5 @@ abbr --add gotstw "gotestsum --watch --format-hide-empty-pkg -f testname -- -p=1
 
 # other stuff
 abbr --add check_ping "ping -c 1 -W 3 google.com"
-abbr --add jqcs       "jq 'map_keys(from_camel|to_snake)'"
-abbr --add cargoall   "cargo install (cargo install --list | grep -E '^[a-z0-9_-]+ v[0-9.]+:?\$' | cut -f1 -d' ')"
+abbr --add jqcs "jq 'map_keys(from_camel|to_snake)'"
+abbr --add cargoall "cargo install (cargo install --list | grep -E '^[a-z0-9_-]+ v[0-9.]+:?\$' | cut -f1 -d' ')"
