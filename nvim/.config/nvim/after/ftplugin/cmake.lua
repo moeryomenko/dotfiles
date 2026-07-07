14d8a13f8c2a6f96ece9b1ed5553b7ee23cc3c1f
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2026-07-07 23:51:47 +0300

    Clean up unused stow packages and extract mako/swappy from sway
    Remove 11 stow packages that are no longer used:
    aerospace, crush, foot, himalaya, kitty, nvim, spacebar, vim,
    waybar, wlogout, yabai
    
    Extract mako and swappy configs from the inactive sway package
    into their own top-level stow packages so they can be deployed
    independently.
    
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

--- nvim/.config/nvim/after/ftplugin/cmake.lua
+++
@@ -1,7 +0,0 @@
-vim.opt_local.expandtab = true
-vim.opt_local.shiftwidth = 2
-vim.opt_local.tabstop = 2
-vim.opt_local.softtabstop = 2
-vim.opt_local.spell = false
-vim.bo.formatoptions = vim.bo.formatoptions .. "croq"
-vim.bo.formatprg = "cmakefmt"
--- nvim/.config/nvim/init.lua
+++
@@ -1,7 +0,0 @@
-require("colors.nord").setup()
-require("core.options")
-require("core.functions")
-require("core.keymaps")
-require("core.autocmds")
-require("core.lazy")
-require("core.lsp")
