363a2a47a5d42e5feb78154f159c2c2341e9803b
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2026-08-27 19:27:57 +0300

    nvim: fix helm-ls binary name in lsp config
    The helm_ls.lua config invokes the server as 'helm_ls' with an
    underscore, but the actual binary is 'helm-ls' with a hyphen. As a
    result the LSP fails to start and helm files get no language server.
    
    Rename the command to 'helm-ls' and reformat the file with tabs and
    a trailing newline to match the repo style.
    
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

--- nvim/.config/nvim/lsp/helm_ls.lua
+++ nvim/.config/nvim/lsp/helm_ls.lua
@@ -1,12 +1,13 @@
 return {
-  cmd = {'helm_ls', 'serve'},
-  filetypes = {'helm'},
-  root_markers = {'Chart.yaml', '.git'},
-  settings = {
-    ['helm-ls'] = {
-      yamlls = {
-        path = 'yaml-language-server',
-      },
-    },
-  },
+	cmd = { 'helm-ls', 'serve' },
+	filetypes = { 'helm' },
+	root_markers = { 'Chart.yaml', '.git' },
+	settings = {
+		['helm-ls'] = {
+			yamlls = {
+				path = 'yaml-language-server',
+			},
+		},
+	},
 }
+
