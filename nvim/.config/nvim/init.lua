e4ec38a4ce2930461614b007b2d97e27ad2f7ff9
Maxim Eryomenko <maxim_eryomenko@rambler.ru>
2026-09-02 12:41:52 +0300

    nvim: remove after deep integraion elio with helix
    Signed-off-by: Maxim Eryomenko <maxim_eryomenko@rambler.ru>

--- nvim/.config/nvim/init.lua
+++
@@ -1,6 +0,0 @@
-require("core.options")
-require("core.functions")
-require("core.keymaps")
-require("core.autocmds")
-require("core.lazy")
-require("core.lsp")
--- nvim/.config/nvim/lsp/neocmakelsp.lua
+++
@@ -1,5 +0,0 @@
-return {
-  cmd = {'neocmakelsp', 'stdio'},
-  filetypes = {'cmake'},
-  root_markers = {'CMakeLists.txt', '.git'},
-}
